2b1018d846e7fc979142175d848235dc4051d2ce
Rsrikako <ravikiran4400@gmail.com>
2025-07-18 18:54:37 +0530

    lint issue fix

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { sendPushNotification } from '../services/sendPushNotification';
-import AppIcon from './AppIcon';
 import { db } from '../services/firebase';
 import { useApartment } from '../context/ApartmentContext';
 import { useAuth } from '../context/AuthContext';
