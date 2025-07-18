329f7d1bd692754a1de4c2d0f679ef7df00cdb68
Rsrikako <ravikiran4400@gmail.com>
2025-07-18 20:12:08 +0530

    configured servie worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,10 +2,10 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY", // TODO: Replace with your Firebase config
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC77ul8fTI1otPz7rB9HLQ5vfP27BgOqu4", // TODO: Replace with your Firebase config
+  projectId: "apartment-maintenance-81c7c",
+  messagingSenderId: "396157322515",
+  appId: "1:396157322515:web:a6946fc576f6847c5246e6"
 });
 
 const messaging = firebase.messaging();
@@ -13,6 +13,6 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage(function(payload) {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
-    icon: '/vite.svg', // You can change this to your app icon
+    icon: '/icon-512x512.png',
   });
 });
